0a23aa556ebbbceb628eec068470650f518b4b43
Felipe Jung <felipemattoseu@gmail.com>
2025-02-19 20:30:21 -0400

    Add edit answer use case

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -20,6 +20,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     return answer;
   }
 
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id);
+
+    this.items[itemIndex] = answer;
+
+    return answer;
+  }
+
   async delete(answer: Answer) {
     this.items = this.items.filter((item) => item.id !== answer.id);
   }
