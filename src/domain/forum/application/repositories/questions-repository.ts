9db97d0007511f089888370abd05526dba6f80dc
Felipe Jung <felipemattoseu@gmail.com>
2025-02-21 12:07:03 -0400

    Add fetch recent questions use case

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -1,9 +1,11 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>;
-  create(question: Question): Promise<Question>;
   findBySlug(slug: string): Promise<Question | null>;
+  findManyRecent({ page }: PaginationParams): Promise<Question[]>;
+  create(question: Question): Promise<Question>;
   save(question: Question): Promise<Question>;
   delete(question: Question): Promise<void>;
 }
