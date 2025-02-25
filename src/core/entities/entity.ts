f8e570b250d680b921f880c6ce1c3062d3d7a5b7
Felipe Jung <felipemattoseu@gmail.com>
2025-02-25 13:21:54 -0400

    Add domain events

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -12,4 +12,8 @@ export abstract class Entity<Props> {
     this.props = props;
     this._id = id ?? new UniqueEntityId();
   }
+
+  public equals(entity: Entity<any>) {
+    return entity === this || entity.id === this._id;
+  }
 }
